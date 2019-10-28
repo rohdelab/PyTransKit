a8290aa25d0b75ba1b1c59b89e196066f10881b6
Xuwang Yin <xuwangyin@gmail.com>
2019-10-28 15:52:22 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
   name = 'pytranskit',
   packages = ['pytranskit'],
-  version = '0.1',
+  version = '0.01',
   license='GNU GPL',
-  description = 'Python transport based signal processing toolkit',
+  description = 'Python transport based signal processing toolkit.',
+  long_description=readme,
+  long_description_content_type='text/markdown',
   author = 'Abu Hasnat Mohammad Rubaiyat, Xuwang Yin, Liam Cattell',
   author_email = 'ar3fx@virginia.edu, xuwangyin@gmail.com',
   url = 'https://github.com/rohdelab/PyTransKit',
-  download_url = 'https://github.com/rohdelab/PyTransKit/archive/0.1.tar.gz',
+  download_url = 'https://github.com/rohdelab/PyTransKit/archive/0.01.tar.gz',
   keywords = ['optimal transport', 'cumulative distribution transform', 'signal processing', 'pattern recognition'],
   install_requires=[
           'six',
@@ -21,7 +27,9 @@ setup(
     'Development Status :: 3 - Alpha',
 
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
