d7cef9e140d76c0f616b281987b720c1632a6bbd
Xuwang Yin <xuwangyin@gmail.com>
2019-10-28 20:48:15 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description=fh.read()
 
 setuptools.setup(
-  name = 'pytranskit',
-  packages = ['pytranskit'],
-  version = '0.01',
-  license='GNU GPL',
-  description = 'Python transport based signal processing toolkit.',
+  name="pytranskit",
+  version="0.0.1",
+  author="Abu Hasnat Mohammad Rubaiyat, Xuwang Yin, Liam Cattell",
+  author_email="ar3fx@virginia.edu, xuwangyin@gmail.com",
+  description="Python transport based signal processing toolkit.",
   long_description=long_description,
-  long_description_content_type='text/markdown',
-  author = 'Abu Hasnat Mohammad Rubaiyat, Xuwang Yin, Liam Cattell',
-  author_email = 'ar3fx@virginia.edu, xuwangyin@gmail.com',
-  url = 'https://github.com/rohdelab/PyTransKit',
-  download_url = 'https://github.com/rohdelab/PyTransKit/archive/0.01.tar.gz',
-  keywords = ['optimal transport', 'cumulative distribution transform', 'signal processing', 'pattern recognition'],
-  install_requires=[
-          'six',
-          'scikit-image',
-          'scipy',
-          'numpy',
-          'scikit-learn',
-      ],
+  long_description_content_type="text/markdown",
+  url="https://github.com/rohdelab/PyTransKit",
+  packages=setuptools.find_packages(),
   classifiers=[
-    'Development Status :: 3 - Alpha',
+    "Development Status :: 3 - Alpha",
 
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
   ],
+  python_requires='>=3.6',
 )
