a96820c2fc4bb44c12dba93c7fdaa35be17c83cc
ar3fx <ar3fx@d-172-18-71-63.vpn.net.virginia.edu>
2021-12-21 01:57:37 -0500

    updated version to 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="pytranskit",
-  version="0.2.2",
+  version="0.2.3",
   author="Abu Hasnat Mohammad Rubaiyat, Xuwang Yin, Liam Cattell, Soheil Kolouri, Mohammad Shifat-E-Rabbi, Yan Zhuang, Gustavo Rohde",
   author_email="ar3fx@virginia.edu, xuwangyin@gmail.com, gustavo.rohde@gmail.com",
   description="Python transport based signal processing toolkit.",
